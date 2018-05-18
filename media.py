a388ee51b5da4a8da1eba2aa981fb95c83e112e6
Arie3301 <arie_slobbe@hotmail.com>
2018-05-18 14:30:01 +0200

    format code

--- media.py
+++ media.py
@@ -1,7 +1,24 @@
+Copyright (c) 2018, Arie Slobbe
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 import webbrowser
 
+
 class Movie():
-    """Create an instance of a movie with associated data."""
+    """Create an instance of a movie with associated data.
+
+    Attributes:
+        title: A string with the title of the movie.
+        storyline: A string with the storyline of the movie.
+        poster_image_url: A string with a url to the movie's poster.
+        trailer_youtube_url: A string with a url to a YouTube video of the movie's trailer.
+    """
+
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
@@ -9,4 +26,5 @@ class Movie():
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """Open the trailer URL in a browser."""
         webbrowser.open(self.trailer_youtube_url)
