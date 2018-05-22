b89f7d8e2d8e21ef158be089a8581e0232d0b7ce
Arie3301 <arie_slobbe@hotmail.com>
2018-05-22 16:39:46 +0200

    Code was formatted according to PEP8 guidelines.

--- entertainmentCenter.py
+++ entertainmentCenter.py
@@ -4,14 +4,58 @@ import fresh_tomatoes
 
 def main():
     """Generate an HTML page to show movies with corresponding information."""
-    toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
-    forrest_gump = media.Movie("Forrest Gump", "A story of an imbecile who makes history.", "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg", "https://www.youtube.com/watch?v=77ij5gCTjYU")
-    goodfellas = media.Movie("Goodfellas", "The rise and fall of a gangster.", "https://upload.wikimedia.org/wikipedia/en/7/7b/Goodfellas.jpg", "https://www.youtube.com/watch?v=qo5jJpHtI1Y")
-    lion_king = media.Movie("The Lion King", "Hamlet on the Savanah.", "https://upload.wikimedia.org/wikipedia/en/3/3d/The_Lion_King_poster.jpg", "https://www.youtube.com/watch?v=4sj1MT05lAA")
-    hercules = media.Movie("Hercules", "The son of Zeus must earn his place back among the gods.", "https://upload.wikimedia.org/wikipedia/en/6/65/Hercules_%281997_film%29_poster.jpg", "https://www.youtube.com/watch?v=ZvtspevZxpg")
-    oceans_11 = media.Movie("Ocean's Eleven", "A gang of eleven makes a casino heist.", "https://upload.wikimedia.org/wikipedia/en/6/68/Ocean%27s_Eleven_2001_Poster.jpg", "https://www.youtube.com/watch?v=imm6OR605UI")
+    toy_story = media.Movie(
+        "Toy Story",
+        "A story of a boy and his toys that come to life.",
+        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+        "https://www.youtube.com/watch?v=KYz2wyBy3kc"
+        )
 
-    movies = [toy_story, forrest_gump, goodfellas, lion_king, hercules, oceans_11]
+    forrest_gump = media.Movie(
+        "Forrest Gump",
+        "A story of an imbecile who makes history.",
+        "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg",  # NOQA
+        "https://www.youtube.com/watch?v=77ij5gCTjYU"
+        )
+
+    goodfellas = media.Movie(
+        "Goodfellas",
+        "The rise and fall of a gangster.",
+        "https://upload.wikimedia.org/wikipedia/en/7/7b/Goodfellas.jpg",
+        "https://www.youtube.com/watch?v=qo5jJpHtI1Y"
+        )
+
+    lion_king = media.Movie(
+        "The Lion King",
+        "Hamlet on the Savanah.",
+        "https://upload.wikimedia.org/wikipedia/en/3/3d/The_Lion_King_poster.jpg",  # NOQA
+        "https://www.youtube.com/watch?v=4sj1MT05lAA"
+        )
+
+    hercules = media.Movie(
+        "Hercules",
+        "The son of Zeus must earn his place back among the gods.",
+        "https://upload.wikimedia.org/wikipedia/en/6/65/Hercules_%281997_film%29_poster.jpg",  # NOQA
+        "https://www.youtube.com/watch?v=ZvtspevZxpg"
+        )
+
+    oceans_11 = media.Movie(
+        "Ocean's Eleven",
+        "A gang of eleven makes a casino heist.",
+        "https://upload.wikimedia.org/wikipedia/en/6/68/Ocean%27s_Eleven_2001_Poster.jpg",  # NOQA
+        "https://www.youtube.com/watch?v=imm6OR605UI"
+        )
+
+    # fresh_tomatoes.open_movies_page() requires its argument in list format
+    movies = [
+        toy_story,
+        forrest_gump,
+        goodfellas,
+        lion_king,
+        hercules,
+        oceans_11
+        ]
+
     fresh_tomatoes.open_movies_page(movies)
 
 
--- media.py
+++ media.py
@@ -8,10 +8,12 @@ class Movie():
         title: A string with the title of the movie.
         storyline: A string with the storyline of the movie.
         poster_image_url: A string with a url to the movie's poster.
-        trailer_youtube_url: A string with a url to a YouTube video of the movie's trailer.
+        trailer_youtube_url: A string with a url to a YouTube video of the
+            movie's trailer.
     """
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
